ec7c2c0bdafe1ac3efe77d1162347d271236a7ac
samjeffcoat <samjeffcoat@gmail.com>
2019-02-20 16:27:20 -0600

    Adds the ability to find a post by specific id

--- routers/posts-router.js
+++ routers/posts-router.js
@@ -4,17 +4,35 @@ const Posts = require("../data/helpers/postDb");
 
 const router = express.Router();
 
+//get posts
 router.get("/", async (req, res) => {
   try {
     const posts = await Posts.get();
     res.status(200).json(posts);
   } catch (error) {
-    res
-      .status(500)
-      .json({
-        error: "The posts information you are looking for cannot be found"
-      });
+    res.status(500).json({
+      error: "The posts information you are looking for cannot be found"
+    });
   }
 });
 
+//GEt Posts by ID
+
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Posts.getById(req.params.id);
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The post with that id cannot be found, sir!" });
+    }
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "That post information could not be found!" });
+  }
+});
 module.exports = router;
