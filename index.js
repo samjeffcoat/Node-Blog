d6957d4b3a96272a3ada332042ee8994367c56db
samjeffcoat <samjeffcoat@gmail.com>
2019-02-21 13:08:51 -0600

    Adds server to deploy to heroku

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-// code away!
-const server = require ('./server.js');
+require("dotenv").config();
+const server = require("./server.js");
 
+const port = process.env.PORT || 5000;
 
-
-
-
-server.listen(5555, () => {
-    console.log('\n* Server Running on http://localhost:5555 *\n');
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
