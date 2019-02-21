0f58c9ce58a3f371edae2e39411ded29679e78ad
samjeffcoat <samjeffcoat@gmail.com>
2019-02-20 19:02:23 -0600

    Adds the ability to find a user by Id

--- routers/users-router.js
+++ routers/users-router.js
@@ -17,4 +17,24 @@ router.get("/", async (req, res) => {
   }
 });
 
+// get users by id
+
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await db.getById(req.params.id);
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The user with that id cannot be found, good sit@" });
+    }
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "That post information could not be found" });
+  }
+});
+
 module.exports = router;
